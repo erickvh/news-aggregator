f3641ba75c21d9732222ed93444c01bf4a875a65
Erick Ventura <erick94111@gmail.com>
2020-06-03 17:51:09 -0600

    added main route for news

--- src/news/news.module.ts
+++ src/news/news.module.ts
@@ -1,5 +1,6 @@
 import { Module, HttpModule } from '@nestjs/common';
 import { NewsService } from './news.service';
+import { NewsController } from './news.controller';
 
-@Module({ imports: [HttpModule], providers: [NewsService] })
+@Module({ imports: [HttpModule], providers: [NewsService], controllers: [NewsController] })
 export class NewsModule {}
