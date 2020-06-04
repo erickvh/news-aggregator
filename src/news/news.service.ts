b88da5c18473c16544d36efebc2c0e1b03b4771d
Erick Ventura <erick94111@gmail.com>
2020-06-04 10:55:09 -0600

    defining q param and search for service guardian

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { New } from '../interfaces/New.interface';
+import { map } from 'rxjs/operators';
 
 @Injectable()
-export class NewsService {}
+export class NewsService {
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  getGuardianArticles(searchContent: string) {
+    const apiKey = this.configService.get('API_KEY_GUARDIAN');
+    const filterNewest = 'order-by=newest&show-fields=byline';
+    return this.httpService
+      .get(
+        `https://content.guardianapis.com/search?api-key=${apiKey}&q=${searchContent}&${filterNewest}`,
+      )
+      .pipe();
+  }
+}
