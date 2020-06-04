ae4f9316fb4f13900402c248cdbabd7caf096687
Erick Ventura <erick94111@gmail.com>
2020-06-04 13:08:36 -0600

    added service for nyt and guardian source

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -2,6 +2,7 @@ import { Injectable, HttpService } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { map } from 'rxjs/operators';
 import { parseGuardian } from 'src/parsers/guardianParse';
+import { NYTParse } from 'src/parsers/NYTParse';
 
 @Injectable()
 export class NewsService {
@@ -18,5 +19,27 @@ export class NewsService {
         `https://content.guardianapis.com/search?api-key=${apiKey}&q=${searchContent}&${filterNewest}`,
       )
       .pipe(map(response => response.data.response.results.map(parseGuardian)));
+  }
+
+  getNYTimesArticles(searchContent: string) {
+    const apiKey = this.configService.get('API_KEY_NYT');
+    const filterNewest = 'sort=newest';
+
+    return this.httpService
+      .get(
+        `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${apiKey}&q=${searchContent}&filter&${filterNewest}`,
+      )
+      .pipe(map(response => response.data.response.docs.map(NYTParse)));
+  }
+
+  getArticles(searchContent: string, source: string) {
+    switch (source) {
+      case 'nyt':
+        return this.getNYTimesArticles(searchContent);
+        break;
+      case 'guardian':
+        return this.getGuardianArticles(searchContent);
+        break;
+    }
   }
 }
