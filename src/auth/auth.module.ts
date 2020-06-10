ca82990ec4795763b4e9d1e12f8b1045e636fe1d
Erick Ventura <erick94111@gmail.com>
2020-06-10 14:22:07 -0600

    imported module users into auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  imports: [UsersModule],
 })
-export class AuthModule {}
+export class AuthModule {
+  constructor(private readonly usersService: UsersService) {}
+}
