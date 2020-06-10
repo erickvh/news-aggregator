b418e089e4d23723fe0d263da9ae189ecbea604f
Erick Ventura <erick94111@gmail.com>
2020-06-10 11:55:19 -0600

    defined user entity on users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,23 @@ import { NewsModule } from './news/news.module';
 import { EmptySearchMiddleware } from './middleware/empty-search.middleware';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/users.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: parseInt(process.env.PORT_DB),
+      username: process.env.USER_DB,
+      password: process.env.PASSWORD_DB,
+      database: process.env.NAME_DB,
+      synchronize: true,
+      host: process.env.HOST_DB,
+      entities: [User],
     }),
     NewsModule,
     AuthModule,
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+}
