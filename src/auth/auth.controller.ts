9a59865077c9685a22f835227686c68287067662
Erick Ventura <erick94111@gmail.com>
2020-06-10 15:25:14 -0600

    added register user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { AuthDTO } from './dto/auth.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('')
 export class AuthController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Post('register')
-  async register(@Body() AuthDTO) {
-    return 'algo';
+  async register(@Body() authDTO: AuthDTO) {
+    try {
+      return await this.usersService.createUser(authDTO);
+    } catch (error) {
+      throw new UnprocessableEntityException();
+    }
   }
 }
