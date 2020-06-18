4b3b5c5978863ec074a8b99b35e255afca4d7107
Erick Ventura <erick94111@gmail.com>
2020-06-18 14:47:55 -0600

    response valid for multiple sources

--- src/news/news.module.ts
+++ src/news/news.module.ts
@@ -1,6 +1,20 @@
 import { Module, HttpModule } from '@nestjs/common';
 import { NewsService } from './news.service';
 import { NewsController } from './news.controller';
+import { StrategyContext } from './strategies/context.strategy';
+import { GuardianStrategy } from './strategies/guardian.strategy';
+import { NYTStrategy } from './strategies/nytapi.strategy';
+import { NewsAPIStrategy } from './strategies/newsapi.strategy';
 
-@Module({ imports: [HttpModule], providers: [NewsService], controllers: [NewsController] })
+@Module({
+  imports: [HttpModule],
+  providers: [
+    NewsService,
+    NewsAPIStrategy,
+    StrategyContext,
+    GuardianStrategy,
+    NYTStrategy,
+  ],
+  controllers: [NewsController],
+})
 export class NewsModule {}
